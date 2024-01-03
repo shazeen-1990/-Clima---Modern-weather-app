13bcf8567d56bfa56ad432e5dba32b0b3a41a808
Shazeen Thowfeek <114829290+shazeen-1990@users.noreply.github.com>
2024-01-03 16:38:46 +0530

    Add files via upload

---
+++ lib/services/location.dart
@@ -0,0 +1,44 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  late double latitude;
+  late double longitude;
+
+  Future<Position> getCurrentLocation() async{
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    }catch (e){
+      print(e);
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+}
